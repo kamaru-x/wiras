502e9a3d668929b132237f48db79303e4dbb1315
Kamaru <xin.kamaru@gmail.com>
2023-06-06 19:46:56 +0530

    /// design changes in footer ///

--- FrontPage/context_processors.py
+++ FrontPage/context_processors.py
@@ -1,5 +1,8 @@
 from Core.models import Department
+from datetime import datetime
 
 def extras(request):
     departments = Department.objects.filter(Status=1)
-    return {'departments':departments}
+    date = datetime.today()
+    year = date.year
+    return {'departments':departments,'year':year}
