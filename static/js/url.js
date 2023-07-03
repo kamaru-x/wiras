a23be981d730a27fd8140346099fc892e662fcae
Kamaru <xin.kamaru@gmail.com>
2023-07-03 19:59:28 +0530

    /// change _ to - ///

--- static/js/url.js
+++ static/js/url.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('#title').on('keyup',function(){
         var value = $('#title').val();
-        var final = value.replace(/ /g,"_");
+        var final = value.replace(/ /g,"-");
         $('#seo_url').val(final);
     });
 })
